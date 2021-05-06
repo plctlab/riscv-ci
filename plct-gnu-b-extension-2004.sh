60c07deb0ea81c383c8e13aa059d21e79858090f
Wei Wu <wuwei2016@iscas.ac.cn>
2021-05-06 16:12:31 +0800

    upload plct-gnu-b-extension-2004.sh

---
+++ plct-gnu-b-extension-2004.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/riscv/riscv-gnu-toolchain 
+cd riscv-gnu-toolchain
+git submodule update --init
+cd riscv-gcc
+git remote add jw https://github.com/pz9115/riscv-gcc.git
+git fetch jw
+git checkout jw/riscv-gcc-10.2.0-rvb
+cd ../riscv-binutils
+git remote add jw https://github.com/pz9115/riscv-binutils-gdb.git
+git fetch jw
+git checkout jw/riscv-binutils-experiment
+
+# test:
+cd ..
+./configure --prefix="$PWD/opt-riscv/" --enable-multilib=true
+
+# you can use make -j* to make speed up
+make report-gcc-newlib -j $(nproc)
+make report-binutils-newlib -j $(nproc)
