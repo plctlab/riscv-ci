a353bab2ecdfd7f669fe22a62ccdf39208e28796
Wei Wu <wuwei2016@iscas.ac.cn>
2021-05-06 16:13:40 +0800

    upload plct-gnu-zfinx-rv32E.sh

---
+++ plct-gnu-zfinx-rv32E.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#apt-get install git build-essential tcl expect flex texinfo bison libpixman-1-dev libglib2.0-dev pkg-config zlib1g-dev ninja-build
+
+#git clone https://github.com/riscv/riscv-gnu-toolchain 
+#cd riscv-gnu-toolchain
+git submodule update --init
+#cd ..
+#cp riscv-gnu-toolchain zfinx -r && cd zfinx
+
+cd riscv-gcc
+git remote | grep -q zfinx || git remote add zfinx https://github.com/pz9115/riscv-gcc.git
+git fetch zfinx
+git checkout -f zfinx/riscv-gcc-10.2.0-zfinx
+cd ../riscv-binutils
+git remote | grep -q zfinx || git remote add zfinx https://github.com/pz9115/riscv-binutils-gdb.git
+git fetch zfinx
+git checkout zfinx/riscv-binutils-2.35-zfinx
+cd ../qemu
+git remote | grep -q plct-qemu || git remote add plct-qemu https://github.com/isrc-cas/plct-qemu.git || true
+git fetch plct-qemu
+git checkout plct-qemu/plct-zfinx-dev
+git reset --hard d73c46e4a84e47ffc61b8bf7c378b1383e7316b5
+
+cd ..
+
+
+# for rv32e:
+./configure --prefix=$PWD/opt-rv32e/ --with-arch=rv32ec --with-abi=ilp32e --with-multilib-generator="rv32ec-ilp32e--"
+
+# you can use make -j* to make speed up
+make -j $(nproc) report-gcc-newlib
+make -j $(nproc) report-binutils-newlib
+
