7e90901bc757d5d301bb094fd2ad0cfcd18e4ea1
jiawei <jiawei@iscas.ac.cn>
2021-05-10 16:41:53 +0900

    Update plct-gnu-b-extensions arch options

--- plct-gnu-b-extension-1804.sh
+++ plct-gnu-b-extension-1804.sh
@@ -10,11 +10,16 @@ git remote add jw https://github.com/pz9115/riscv-binutils-gdb.git
 git fetch jw
 git checkout jw/riscv-binutils-experiment
 
-# test:
+# test rv64:
 cd ..
-./configure --prefix="$PWD/opt-riscv/" --enable-multilib=true
+./configure --prefix="$PWD/opt-riscv/" --with-arch=rv64gcb --with-abi=lp64d --with-mulitilib-generator="rv64gcb-lp64d--"
 
 # you can use make -j* to make speed up
 # Remove GCC test due to b-ext is not support completely, we will restart it after B-ext GCC part finish by Sifive
 # make report-gcc-newlib -j $(nproc)
+make report-binutils-newlib -j $(nproc)
+
+# test rv32:
+./configure --prefix="$PWD/opt-riscv/" --with-arch=rv32gcb --with-abi=ilp32d --with-mulitilib-generator="rv32gcb-ilp32d--"
+make clean 
 make report-binutils-newlib -j $(nproc)
--- plct-gnu-b-extension-2004.sh
+++ plct-gnu-b-extension-2004.sh
@@ -12,9 +12,14 @@ git checkout jw/riscv-binutils-experiment
 
 # test:
 cd ..
-./configure --prefix="$PWD/opt-riscv/" --enable-multilib=true
+./configure --prefix="$PWD/opt-riscv/" --with-arch=rv64gcb --with-abi=lp64d --with-mulitilib-generator="rv64gcb-lp64d--"
 
 # you can use make -j* to make speed up
 # Remove GCC test due to b-ext is not support completely, we will restart it after B-ext GCC part finish by Sifive
 # make report-gcc-newlib -j $(nproc)
+make report-binutils-newlib -j $(nproc)
+
+# test rv32:
+./configure --prefix="$PWD/opt-riscv/" --with-arch=rv32gcb --with-abi=ilp32d --with-mulitilib-generator="rv32gcb-ilp32d--"
+make clean 
 make report-binutils-newlib -j $(nproc)
