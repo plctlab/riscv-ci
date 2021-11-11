ece37213b5dfc090de4a3944d03a5c71832bfe29
LiaoShihua <shihua@iscas.ac.cn>
2021-11-11 17:35:10 +0800

    fixed error about zceb.sh

--- plct-gnu-zceb.sh
+++ plct-gnu-zceb.sh
@@ -10,17 +10,18 @@ cd ../riscv-binutils
 
 git remote add lin https://github.com/linsinan1995/riscv-binutils-gdb.git
 git fetch lin 
-git checkout lin/riscv-binutils-experiment-zceb
+git checkout lin/riscv-binutils-experiment-zceb@cleanup
 
 cd ../qemu
 git remote add plctlab https://github.com/plctlab/plct-qemu.git
 git fetch plctlab
 git checkout plctlab/plct-zce-dev
 
+cd ..
 
 sed -i '15c qemu-riscv$xlen -cpu rv64,x-zce=true -r 5.10 "${qemu_args[@]}" -L ${RISC_V_SYSROOT} "$@"' scripts/wrapper/qemu/riscv64-unknown-elf-run 
 
 
-./configure --prefix="$PWD/opt-riscv-rv64zceb" --with-arch=rv64imac_zceb --with-abi=lp64d --with-multilib-generator="rv64imac_zceb-lp64d--"
+./configure --prefix="$PWD/opt-riscv-rv64zceb" --with-arch=rv64imac_zceb --with-abi=lp64 --with-multilib-generator="rv64imac_zceb-lp64--"
 make report-gcc-newlib -j $(nproc)
 make report-binutils-newlib -j $(nproc)
