ddc8418d81877737b36fc25c33e5462fb23da716
Lu Yahan <yahan@iscas.ac.cn>
2021-11-15 18:13:09 +0800

    Update node-riscv64.sh

--- node-riscv64.sh
+++ node-riscv64.sh
@@ -2,7 +2,7 @@
 
 # Add riscv64 toolchain into PATH
 export PATH="$PATH:/opt/riscv/bin"
-riscv64-unknown-linux-gnu-g++ -v
+which riscv64-unknown-linux-gnu-gcc
 if [ $? -ne 0 ]; then
     wget https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2021.09.21/riscv64-glibc-ubuntu-18.04-nightly-2021.09.21-nightly.tar.gz && \
          tar xzvf riscv64-glibc-ubuntu-18.04-nightly-2021.09.21-nightly.tar.gz
