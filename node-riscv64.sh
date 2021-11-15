21d49c9846e2517580a0da68e904fb7b9b563257
Lu Yahan <yahan@iscas.ac.cn>
2021-11-15 14:40:28 +0800

    Update node-riscv64.sh

--- node-riscv64.sh
+++ node-riscv64.sh
@@ -7,8 +7,8 @@ git clone https://github.com/nodejs/node.git
 cd node
 export CC=riscv64-unknown-linux-gnu-gcc
 export CXX=riscv64-unknown-linux-gnu-g++
-export CC_host=clang
-export CXX_host=clang++
+export CC_host=gcc
+export CXX_host=g++
 
 ./configure --cross-compiling --dest-cpu=riscv64  --verbose --openssl-no-asm
 make -j$(nproc)
