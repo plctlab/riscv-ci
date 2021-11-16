610f5873fab43651cfd855906380048a141e4148
Lu Yahan <yahan@iscas.ac.cn>
2021-11-16 10:20:56 +0800

    Update node-riscv64.sh

--- node-riscv64.sh
+++ node-riscv64.sh
@@ -6,13 +6,16 @@ wget https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2021.
      tar xzvf riscv64-glibc-ubuntu-20.04-nightly-2021.09.21-nightly.tar.gz  -C $HOME/
 export PATH="$PATH:$HOME/riscv/bin/"
 
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz && \
+     tar -xf clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz -C $HOME/
+export PATH="$PATH:$HOME/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04/bin/"
 
 git clone https://github.com/v8-riscv/node.git
 cd node
 export CC=riscv64-unknown-linux-gnu-gcc
 export CXX=riscv64-unknown-linux-gnu-g++
-export CC_host=gcc
-export CXX_host=g++
+export CC_host=clang
+export CXX_host=clang++
 
 ./configure --cross-compiling --dest-cpu=riscv64  --verbose --openssl-no-asm
 make -j$(nproc)
