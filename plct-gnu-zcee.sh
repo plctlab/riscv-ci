4ca18b56b35b9ef3eb96003914ec34c8e126ee33
yulong-plct <yulong@nj.iscas.ac.cn>
2021-12-02 11:49:52 +0800

    change branch zcee

--- plct-gnu-zcee.sh
+++ plct-gnu-zcee.sh
@@ -2,15 +2,15 @@ git clone https://github.com/pz9115/riscv-gnu-toolchain.git
 cd riscv-gnu-toolchain
 git submodule update --init
 cd riscv-gcc
-git remote add lsh https://github.com/LiaoShiHua/riscv-gcc.git
-git fetch lsh
-git checkout lsh/riscv-gcc-zcee
+git remote add syl https://github.com/pz9115/corev-gcc.git
+git fetch syl
+git checkout syl/development
 
 cd ../riscv-binutils
 
-git remote add lin https://github.com/linsinan1995/riscv-binutils-gdb.git
-git fetch lin 
-git checkout lin/riscv-binutils-experiment-zcee 
+git remote add syl https://github.com/pz9115/corev-binutils-gdb.git
+git fetch syl 
+git checkout syl/development 
 
 cd ../qemu
 git remote add plctlab https://github.com/plctlab/plct-qemu.git
@@ -19,7 +19,7 @@ git checkout plctlab/plct-zce-dev
 
 cd ..
 
-sed -i '15c qemu-riscv$xlen -cpu rv64,x-zce=true -r 5.10 "${qemu_args[@]}" -L ${RISC_V_SYSROOT} "$@"' scripts/wrapper/qemu/riscv64-unknown-elf-run 
+sed -i '15c qemu-riscv$xlen -cpu rv64,x-zcee=true -r 5.10 "${qemu_args[@]}" -L ${RISC_V_SYSROOT} "$@"' scripts/wrapper/qemu/riscv64-unknown-elf-run 
 
 
 ./configure --prefix="$PWD/opt-riscv-rv64zcee" --with-arch=rv64gc_zcee --with-abi=lp64d --with-multilib-generator="rv64gc_zcee-lp64d--"
