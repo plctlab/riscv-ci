304582dee4e3214a9c1d58c90c007b1cf89e8b94
Lu Yahan <yahan@iscas.ac.cn>
2022-03-29 09:42:19 +0800

    Update v8-upstream-master.sh

--- v8-upstream-master.sh
+++ v8-upstream-master.sh
@@ -39,7 +39,7 @@ run_sim_test () {
     shift
   done
 
-  python2 ./tools/run-tests.py $ARGS # 2>&1 | tee "$LOG_FILE.simbuild.$BTYPE.${t}${SUFFIX}"
+  python3 ./tools/run-tests.py $ARGS # 2>&1 | tee "$LOG_FILE.simbuild.$BTYPE.${t}${SUFFIX}"
 }
 
 run_all_sim_build_checks () {
