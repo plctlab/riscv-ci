626fc269cb75b43276b98da72d973a5251334089
Jiawei <jiawei@iscas.ac.cn>
2022-09-05 15:56:55 +0800

    Update ci script
    Update ci script, remove no use script. Track upstream branch and RVI branch.

---
+++ gnu-toolchain-rvi.sh
@@ -0,0 +1,21 @@
+git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git
+cd riscv-gnu-toolchain
+git submodule update --init --recursive
+
+# make rv32
+./configure --prefix="$PWD/opt-riscv-rv64/" --with-arch=rv64gc --with-abi=lp64d
+make -j $(nproc)
+
+#test rv32
+make report-gcc-newlib -j $(nproc) &&
+make report-binutils -j $(nproc)
+
+make clean
+
+# make rv32
+./configure --prefix="$PWD/opt-riscv-rv32/" --with-arch=rv32gc --with-abi=ilp32d
+make -j $(nproc)
+
+#test rv32
+make report-gcc-newlib -j $(nproc) &&
+make report-binutils -j $(nproc)
