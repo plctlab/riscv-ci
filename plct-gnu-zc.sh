162de8127c5f46b7cf3d89e08ff1dcc4bae59e82
jiawei <jiawei@iscas.ac.cn>
2022-12-06 16:45:31 +0800

    Update zc repo's url.

--- plct-gnu-zc.sh
+++ plct-gnu-zc.sh
@@ -2,13 +2,13 @@ git clone https://github.com/pz9115/riscv-gnu-toolchain.git
 cd riscv-gnu-toolchain
 git submodule update --init
 cd riscv-gcc
-git remote add corev https://github.com/plctlab/corev-gcc.git
+git remote add corev https://github.com/openhwgroup/corev-gcc.git
 git fetch corev
 git checkout corev/development
 
 cd ../riscv-binutils
 
-git remote add corev https://github.com/plctlab/corev-binutils-gdb.git
+git remote add corev https://github.com/openhwgroup/corev-binutils-gdb.git
 git fetch corev 
 git checkout corev/development 
 
