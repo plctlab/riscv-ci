abe45a14ee622e5438b12033b29670c50b4a0a14
Lu Yahan <yahan@iscas.ac.cn>
2023-03-22 20:13:24 +0800

    Update node-riscv64.sh

--- node-riscv64.sh
+++ node-riscv64.sh
@@ -6,10 +6,14 @@ wget https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2021.
      tar xzvf riscv64-glibc-ubuntu-20.04-nightly-2021.09.21-nightly.tar.gz  -C $HOME/
 export PATH="$PATH:$HOME/riscv/bin/"
 
+rm -rf node
 git clone https://github.com/nodejs/node.git
 cd node 
 git config user.email "you@example.com"
 git config user.name "Your Name"
+
+git log -1
+
 export CC=riscv64-unknown-linux-gnu-gcc
 export CXX=riscv64-unknown-linux-gnu-g++
 export CC_host=gcc
