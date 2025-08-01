3da982793d95f1695e855109e56d95a455be24ba
JiQiu <qiuji@iscas.ac.cn>
2025-08-01 15:53:41 +0800

    Add initial try with qemu

--- v8-upstream-master-fastcheck-riscv64.sh
+++ v8-upstream-master-fastcheck-riscv64.sh
@@ -3,12 +3,37 @@
 set -e
 
 V8_ROOT=$PWD/v8-riscv
-#RV_HOME=/opt/riscv
-QEMU_ADDR=riscv@localhost
+BASE_DIR=$PWD
+SYSROOT=$PWD/riscv/sysroot/
+INSN_PLUGIN=$PWD/install-qemu/plugin/libinsn.so
 
-# Debug. Clean build.
-#rm -rf depot_tools
-#rm -rf "V8_ROOT"
+
+prepare_sysroot () {
+wget https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2024.04.12/riscv64-glibc-ubuntu-22.04-gcc-nightly-2024.04.12-nightly.tar.gz
+
+tar zxvf riscv64-glibc-ubuntu-22.04-gcc-nightly-2024.04.12-nightly.tar.gz
+}
+
+prepare_qemu () {
+mkdir -p "$BASE_DIR/install-qemu"
+
+git clone git@github.com:qemu/qemu.git
+
+cd qemu
+
+git checkout -t remotes/origin/stable-9.0
+
+mkdir build
+cd build
+
+../configure --target-list=riscv64-linux-user --enable-user --enable-slirp --enable-plugins --prefix="$BASE_DIR/install-qemu"
+
+make
+make install
+
+cp -a ./tests/plugin/ "$BASE_DIR/install-qemu"
+cd $BASE_DIR
+}
 
 [ -d depot_tools ] || git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
@@ -42,6 +67,16 @@ run_sim_test () {
   python3 ./tools/run-tests.py $ARGS # 2>&1 | tee "$LOG_FILE.simbuild.$BTYPE.${t}${SUFFIX}"
 }
 
+run_benchmark() {
+# set up qemu path
+   export PATH="$BASE_DIR/install-qemu/bin:$PATH"
+# try qemu-riscv64
+   qemu-riscv64 -h
+# run with proper cmd line
+   cd "$V8_ROOT/v8"
+   qemu-riscv64 -L $SYSROOT -plugin $INSN_PLUGIN -d plugin  ./out/riscv64.sim.release/d8 $V8_ROOT/test/benchmarks/data/sunspider/3d-cube.js
+}
+
 run_all_sim_build_checks () {
   cd "$V8_ROOT/v8"
   # build simulator config
@@ -69,7 +104,7 @@ run_all_sim_build_checks () {
   run_sim_test out/riscv64.sim.release # 2>&1 | tee "$LOG_FILE.sim.release"
   run_sim_test out/riscv64.sim.release stress # 2>&1 | tee "$LOG_FILE.sim.release.stress"
   #run_sim_test out/riscv64.sim.release jitless
-
+  run_benchmark
 }
 
 
@@ -79,6 +114,8 @@ cd "$V8_ROOT/v8"
 git log -1
 
 #run_x86_build_checks
+prepare_sysroot
+prepare_qemu
 run_all_sim_build_checks
 # build_cross_builds
 # run_on_qemu
