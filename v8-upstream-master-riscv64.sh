0bc8277169867e4a4be87326eb848fb27f6deeec
Kasper Lund <kasperl@rivosinc.com>
2025-08-21 19:08:26 +0200

    Fix typo (repeated scheme) in git repo url

--- v8-upstream-master-riscv64.sh
+++ v8-upstream-master-riscv64.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git -C riscv-ci pull || git clone https://https://github.com/plctlab/riscv-ci riscv-ci
+git -C riscv-ci pull || git clone https://github.com/plctlab/riscv-ci riscv-ci
 python3 $PWD/riscv-ci/v8-upstream-master-riscv64.py
