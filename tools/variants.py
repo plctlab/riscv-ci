55bdce048e1fdc6c2d03e08b10b408ab82b16bd0
Kasper Lund <kasperl@rivosinc.com>
2025-08-22 14:13:26 +0200

    Translate remaining non-pts scripts to Python

--- tools/variants.py
+++ tools/variants.py
@@ -1,7 +1,7 @@
 import os
 import platform
 
-TARGET_CPU_MAPPING = { "x86_64": "x64" }
+TARGET_CPU_MAPPING = { "x86_64": "x64", "xxx": "x86" }
 TARGET_CPU = TARGET_CPU_MAPPING.get(platform.machine(), platform.machine())
 
 class Variant:
@@ -16,6 +16,16 @@ class Variant:
         lines = self.config.splitlines()
         return " ".join([x.strip() for x in lines if x != ""])
 
+RISCV32_OPTDEBUG_SIM = Variant("riscv32.optdebug.sim", f"""
+  is_component_build=false
+  is_debug=true
+  target_cpu="x86"
+  v8_enable_backtrace=true
+  v8_enable_slow_dchecks=true
+  v8_optimized_debug=true
+  v8_target_cpu="riscv32"
+""")
+
 RISCV64_OPTDEBUG_SIM = Variant("riscv64.optdebug.sim", f"""
   is_component_build=false
   is_debug=true
@@ -26,9 +36,24 @@ RISCV64_OPTDEBUG_SIM = Variant("riscv64.optdebug.sim", f"""
   v8_target_cpu="riscv64"
 """)
 
+RISCV32_RELEASE_SIM = Variant("riscv32.release.sim", f"""
+  is_component_build=false
+  is_debug=false
+  target_cpu="x86"
+  v8_target_cpu="riscv32"
+""")
+
 RISCV64_RELEASE_SIM = Variant("riscv64.release.sim", f"""
   is_component_build=false
   is_debug=false
   target_cpu="{TARGET_CPU}"
   v8_target_cpu="riscv64"
+""")
+
+RISCV64_PTRCOMP_RELEASE_SIM = Variant("riscv64.ptrcomp.release.sim", f"""
+  is_component_build=false
+  is_debug=false
+  target_cpu="{TARGET_CPU}"
+  v8_target_cpu="riscv64"
+  v8_enable_pointer_compression=true
 """)
