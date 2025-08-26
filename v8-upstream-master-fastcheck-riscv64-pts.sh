80148f77d459fb24a586b0c416bdb5a3fb099b02
JiQiu <qiuji@iscas.ac.cn>
2025-08-26 09:51:47 +0800

    Fix bug
    "this CI's total insn" pollutes "the last successful CI's total insn"

--- v8-upstream-master-fastcheck-riscv64-pts.sh
+++ v8-upstream-master-fastcheck-riscv64-pts.sh
@@ -76,14 +76,14 @@ run_get_lastSuccessfulBuild_info() {
   cd "$V8_ROOT/v8"
 # get the lastSuccessfulBuild number
   BUILD_NUM=$(curl -s https://ci.rvperf.org/view/V8/job/v8-upstream-master-fastcheck-riscv64-pts/lastSuccessfulBuild/buildNumber | grep -o '[0-9]*')
-  curl -s "https://ci.rvperf.org/view/V8/job/v8-upstream-master-fastcheck-riscv64-pts/$BUILD_NUM/consoleFull" |  tr -d '\r'  2>&1 |tee lastSuccessfulBuild.log >/dev/null
+  curl -s "https://ci.rvperf.org/view/V8/job/v8-upstream-master-fastcheck-riscv64-pts/$BUILD_NUM/consoleFull" |  tr -d '\r'  > lastSuccessfulBuild.log 2>&1
   wc -l lastSuccessfulBuild.log
   echo "run_get_lastSuccessfulBuild_info $BUILD_NUMBER $BUILD_ID $WORKSPACE Done"
 }
 
 run_cmp_builtinsize() {
   cd "$V8_ROOT/v8"
-  grep -E '^[A-Z]{3} Builtin, ' lastSuccessfulBuild.log 2>&1 |tee logbtsize-lsb.txt >/dev/null
+  grep -E '^[A-Z]{3} Builtin, ' lastSuccessfulBuild.log >logbtsize-lsb.txt 2>&1
   wc -l logbtsize-lsb.txt
   wc -l logbtsize-now.txt
 #  echo "CMP builtin size"
@@ -96,13 +96,14 @@ run_Sunspider() {
   cd "$V8_ROOT/v8/"
   for i in {1..3}; do
     for file in test/benchmarks/data/sunspider/*.js; do
-      echo "Running $(basename "$file")" 2>&1 | tee -a ss-benchmark-${i}.log
-      qemu-riscv64 -L /usr/local/riscv/sysroot -plugin /usr/local/bin/plugin/libinsn.so -d plugin  ./out/riscv64.native.release/d8 "$file" 2>&1 |tee -a ss-benchmark-${i}.log
+      echo "Running $(basename "$file")" >> ss-benchmark-${i}.log 2>&1
+      qemu-riscv64 -L /usr/local/riscv/sysroot -plugin /usr/local/bin/plugin/libinsn.so -d plugin  ./out/riscv64.native.release/d8 "$file" >> ss-benchmark-${i}.log 2>&1
     done
   done
   for i in {1..3}; do
   grep -E "^(Running|total insn)" ss-benchmark-${i}.log | awk '{print($NF)}' | paste -d ' ' - - > ss-result-${i}.log
   done
+  #This line prints the CI result into the log so that the next CI can use this information for comparison."
   awk '{a[FNR]=$1; b[FNR]+=$2} END{for(i=1;i<=FNR;i++) printf("Benchmarking %s\ntotal insn %d\n", a[i], b[i]/3)}' ss-result-1.log ss-result-2.log ss-result-3.log
   awk '{a[FNR]=$1; b[FNR]+=$2} END{for(i=1;i<=FNR;i++) printf("%s %d\n", a[i], b[i]/3)}' ss-result-1.log ss-result-2.log ss-result-3.log >ss-result-now.txt
   ls -al ss-result*
@@ -112,7 +113,7 @@ run_Sunspider() {
 
 run_cmp_Sunspider() {
   cd "$V8_ROOT/v8/"
-  grep -E "^(Benchmarking|total insn)" lastSuccessfulBuild.log | awk '{print($NF)}' | paste -d ' ' - - 2>&1 |tee ss-result-lsb.txt >/dev/null
+  grep -E "^(Benchmarking|total insn)" lastSuccessfulBuild.log | awk '{print($NF)}' | paste -d ' ' - - >ss-result-lsb.txt 2>&1
   wc -l ss-result-lsb.txt
 #  grep -E "^(Benchmarking|total insn)" ss-benchmark.log | awk '{print($NF)}' | paste -d ' ' - - > ss-result-now.txt
   echo "Sunspider lastSuccessfulBuild result: "
@@ -214,4 +215,4 @@ run_get_lastSuccessfulBuild_info
 run_cmp_builtinsize
 #run_JetStream
 run_Sunspider
-run_cmp_Sunspider
+#run_cmp_Sunspider
