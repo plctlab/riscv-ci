e58dd8ea65ef44e0240e5f62073a684dcbf74df4
Kasper Lund <kasperl@rivosinc.com>
2025-08-27 09:59:36 +0200

    Translate pts variant to Python (#121)
    * Start moving pts to Python
    
    * Add missing file
    
    * Fix error
    
    * Allow Python version to fail for now

--- tools/v8.py
+++ tools/v8.py
@@ -18,52 +18,88 @@ GCLIENT_PATH   = os.path.join(DEPOT_TOOLS_DIR, "gclient")
 GN_PATH        = os.path.join(DEPOT_TOOLS_DIR, "gn")
 AUTONINJA_PATH = os.path.join(DEPOT_TOOLS_DIR, "autoninja")
 
+# Cache the last working directory in a variable, so we can print something
+# useful whenever we change the working directory.
+_last_cwd = CWD
 
 # Helper function that runs a command given by the arguments in a subprocess.
 # Notice that we default to checking that it runs successfully and we show
 # useful information about the working directory.
-def exec(arguments, cwd=CWD, check=True):
-    if cwd != CWD: print("+ " + "cd " + cwd, flush=True)
-    commandline = " ".join([f"'{x}'" if " " in x else x for x in arguments])
-    print(f"+ {commandline}", flush=True)
+def _exec(arguments, cwd=CWD, check=True, capture_output=False):
+    global _last_cwd
+    if cwd != _last_cwd:
+        print("+ " + "cd " + cwd, flush=True)
+        _last_cwd = cwd
     # Extend the PATH of the subprocess, so the correct depot_tools are used.
     # This is necessary at least when calling out to tools/run-tests.py.
     env = dict(os.environ)
     env["PATH"] = DEPOT_TOOLS_DIR + os.pathsep + env["PATH"]
-    subprocess.run(arguments, cwd=cwd, check=check, env=env)
-    if cwd != CWD: print("+ " + "cd " + CWD, flush=True)
+    # If we're capturing the output, we redirect stderr to stdout and ask
+    # the subprocess to pipe stdout to us.
+    stdout = None
+    stderr = None
+    if capture_output:
+        stdout = subprocess.PIPE
+        stderr = subprocess.STDOUT
+    # Run the subprocess.
+    commandline = " ".join([f"'{x}'" if " " in x else x for x in arguments])
+    print(f"+ {commandline}", flush=True)
+    process = subprocess.Popen(
+        arguments,
+        cwd=cwd,
+        env=env,
+        stderr=stderr,
+        stdout=stdout,
+        text=True
+    )
+    # Capture the output (if necessary) and write it to stdout as we go along.
+    output = None
+    if capture_output:
+        output = []
+        for line in process.stdout:
+            sys.stdout.write(line)
+            output.append(line)
+    # Wait for the subprocess to terminate and optionally check if the
+    # exit code indicates success.
+    retcode = process.wait()
+    if check and retcode != 0:
+        raise subprocess.CalledProcessError(retcode, arguments)
+    return output
 
 def fetch_depot_tools():
     if os.path.isdir(DEPOT_TOOLS_DIR): return
-    exec(["git", "clone", DEPOT_TOOLS_URL, DEPOT_TOOLS_DIR])
+    _exec(["git", "clone", DEPOT_TOOLS_URL, DEPOT_TOOLS_DIR])
 
 def fetch(clean=False):
     if clean and os.path.isdir(ROOT_DIR):
         shutil.rmtree(ROOT_DIR, ignore_errors=True)
     if not os.path.isdir(ROOT_DIR):
         os.mkdir(ROOT_DIR)
-    v8 = os.path.join(ROOT_DIR, "v8")
-    if os.path.isdir(v8):
+    srcdir = os.path.join(ROOT_DIR, "v8")
+    if os.path.isdir(srcdir):
         # We already have a checked out version of v8, so we assume it is already
         # on the main branch and just pull there.
-        exec(["git", "pull"], cwd=v8)
+        _exec(["git", "pull"], cwd=srcdir)
     else:
         # We do not have a checkout of v8 yet, so we use 'fetch' to get the initial
         # version of it and make sure to change to the main branch.
-        exec([FETCH_PATH, "v8"], cwd=ROOT_DIR)
-        exec(["git", "checkout", "main"], cwd=v8)
-    exec([GCLIENT_PATH, "sync"], cwd=v8)
+        _exec([FETCH_PATH, "v8"], cwd=ROOT_DIR)
+        _exec(["git", "checkout", "main"], cwd=srcdir)
+    _exec([GCLIENT_PATH, "sync"], cwd=srcdir)
 
-def build(variant):
-    v8 = os.path.join(ROOT_DIR, "v8")
-    exec(["git", "log", "-1"], cwd=v8)
+def build(variant, arguments=[]):
+    srcdir = os.path.join(ROOT_DIR, "v8")
+    _exec(["git", "log", "-1"], cwd=srcdir)
     outdir = variant.output_directory()
     args = variant.gn_generate_args()
-    exec([GN_PATH, "gen", outdir, f"--args={args}"], cwd=v8)
-    exec([AUTONINJA_PATH, "-C", outdir], cwd=v8)
+    _exec([GN_PATH, "gen", outdir, f"--args={args}"], cwd=srcdir)
+    _exec([AUTONINJA_PATH, "-C", outdir] + arguments, cwd=srcdir)
+
+def build_d8(variant):
+    build(variant, ["d8"])
 
 def run_tests_specific(variant, tests, *extra):
-    v8 = os.path.join(ROOT_DIR, "v8")
+    srcdir = os.path.join(ROOT_DIR, "v8")
     outdir = variant.output_directory()
     arguments = [
         PYTHON,
@@ -73,7 +109,7 @@ def run_tests_specific(variant, tests, *extra):
         "--rerun-failures-count=2",
         f"--outdir={outdir}",
     ]
-    exec(arguments + list(extra), cwd=v8)
+    _exec(arguments + list(extra), cwd=srcdir)
 
 def run_tests(variant, fast=False, stress=True):
     # The 'default' set of tests is slightly smaller than the 'bot default' set.
@@ -85,3 +121,12 @@ def run_tests(variant, fast=False, stress=True):
     variants = "exhaustive" if stress else "future,default"
     if fast: variants = "stress,default" if stress else "default"
     run_tests_specific(variant, tests, f"--variants={variants}")
+
+def run_d8(variant, arguments, cwd=CWD):
+    srcdir = os.path.join(ROOT_DIR, "v8")
+    outdir = variant.output_directory()
+    return _exec(
+        [os.path.join(srcdir, outdir, "d8")] + arguments,
+        cwd=cwd,
+        capture_output=True
+    )
---
+++ v8-upstream-master-fastcheck-riscv64-pts.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+from urllib.request import urlopen
+
+import tools.v8 as v8
+import tools.variants as variants
+
+URL_BASE = "https://ci.rvperf.org/view/V8/job/v8-upstream-master-fastcheck-riscv64-pts"
+
+# TODO(kasperl@rivosinc.com): Change this to be a native build and run
+# it on qemu-riscv64.
+RISCV64_PTS_RELEASE = variants.Variant("riscv64.pts.release", f"""
+  is_component_build=false
+  is_debug=false
+  target_cpu="{variants.TARGET_CPU}"
+  v8_target_cpu="riscv64"
+  v8_enable_backtrace=true
+  v8_enable_disassembler=true
+  v8_enable_object_print=true
+  v8_enable_verify_heap=true
+""")
+
+# Print information about builtin sizes.
+def report_builtin_sizes():
+    builtin_sizes = v8.run_d8(RISCV64_PTS_RELEASE, [
+            "--print-builtin-size",
+            os.path.join("test", "benchmarks", "data", "sunspider", "3d-cube.js")
+        ],
+        cwd=os.path.join(v8.ROOT_DIR, "v8"))
+    # TODO(kasperl@rivosinc.com): Same output as bash variant for now.
+    print(f"{len(builtin_sizes)} logbtsize-now.txt")
+
+# Gather information about last successful build.
+def last_successful_build_number():
+    success = None
+    with urlopen(f"{URL_BASE}/lastSuccessfulBuild/buildNumber") as response:
+        success = int(response.read().decode('utf-8').strip())
+    with urlopen(f"{URL_BASE}/{success}/consoleFull") as response:
+        lines = response.readlines()
+        # TODO(kasperl@rivosinc.com): Same output as bash variant for now.
+        print(f"{len(lines)} lastSuccessfulBuild.log")
+        BUILD_NUMBER = os.getenv("BUILD_NUMBER", "<unknown>")
+        BUILD_ID = os.getenv("BUILD_ID", "<unknown>")
+        WORKSPACE = os.getenv("WORKSPACE", "<unknown>")
+        print(f"run_get_lastSuccessfulBuild_info "
+              f"{BUILD_NUMBER} {BUILD_ID} {WORKSPACE} "
+              f"Done")
+    return success
+
+# Returns a list of all the SunSpider benchmarks.
+def find_sunspider_benchmarks():
+    return list(glob.iglob(
+        os.path.join("test", "benchmarks", "data", "sunspider", "*.js"),
+        root_dir=os.path.join(v8.ROOT_DIR, "v8")))
+
+# Run a list of benchmarks.
+def run_benchmarks(variant, benchmarks):
+    iterations = 3
+    for iteration in range(iterations):
+        for benchmark in benchmarks:
+            name = os.path.basename(benchmark)
+            print(f"Running {name} - attempt {iteration + 1}/{iterations}", flush=True)
+            v8.run_d8(variant, [benchmark], cwd=os.path.join(v8.ROOT_DIR, "v8"))
+            # TODO(kasperl@rivosinc.com): Gather statistics and report them.
+
+
+v8.fetch_depot_tools()
+v8.fetch()
+v8.build_d8(RISCV64_PTS_RELEASE)
+
+report_builtin_sizes()
+last_successful_build_number()
+sunspider = find_sunspider_benchmarks()
+run_benchmarks(RISCV64_PTS_RELEASE, sunspider)
