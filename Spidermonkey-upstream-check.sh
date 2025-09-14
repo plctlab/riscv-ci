4a9bf89b408ad2b137e0231583f8fadc3d479e2a
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-09-14 14:10:58 +0800

    SpiderMonkey: Don't disable rust-simd while building
    `--enable-rust-simd` is no longer causing build bustages, so we should
    use the default value to achieve higher coverage and better replicate
    real-world scenario.

--- Spidermonkey-upstream-check.sh
+++ Spidermonkey-upstream-check.sh
@@ -26,7 +26,6 @@ ac_add_options --enable-debug
 # switching between optimized and debug builds while developing.
 mk_add_options MOZ_OBJDIR=@TOPSRCDIR@/obj-opt-@CONFIG_GUESS@
 ac_add_options --enable-jitspew
-ac_add_options --disable-rust-simd
 ac_add_options --enable-gczeal
 ac_add_options --enable-simulator=riscv64
 ac_add_options --enable-jit
